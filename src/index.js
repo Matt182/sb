8a8714318196b53b5ae2c4272430d1cd8a8e88fc
Matt182 <mattspring0@gmail.com>
2016-11-08 16:32:23 +0000

    2a

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-function hello(name) {
-  console.log(`Hello ${name}`); // eslint-disable-line
-}
-hello('JS World');
+import express from 'express';
+
+const app = express();
+app.get('/', (req, res) => {
+    console.log('............................................');
+  const a = +req.query.a;
+  const b = +req.query.b;
+  const result = a + b;
+  res.json(result);
+});
+
+app.listen(8080, () => {
+  console.log('listening on port 8080');
+});
