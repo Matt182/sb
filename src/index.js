22a37e1db2718bbba5ae27d65d4616418b04ccd2
Matt182 <mattspring0@gmail.com>
2016-11-08 17:02:28 +0000

    2b

--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ import express from 'express';
 
 const app = express();
 app.get('/', (req, res) => {
-    console.log('............................................');
-  let a = 0;
-  let b = 0;
-  if (req.query.a) {
-    a += +req.query.a;
+  let result = 'Invalid fullname';
+  if (req.query.fullname) {
+    const fio = req.query.fullname.split(' ');
+    if (fio.length < 4) {
+      switch (fio.length) {
+        case 1:
+          result = `${fio[0]}`;
+          break;
+        case 2:
+          result = `${fio[1]} ${fio[0].slice(0, 1)}.`;
+          break;
+        case 3:
+          result = `${fio[2]} ${fio[1].slice(0, 1)}. ${fio[0].slice(0, 1)}.`;
+          break;
+        default:
+      }
+    }
   }
-  if (req.query.b) {
-    b += +req.query.b;
-  }
-  const result = a + b;
   res.json(result);
 });
 
